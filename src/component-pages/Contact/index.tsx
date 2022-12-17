88ddb85575e983fab98eb52aa7a6a5537c1c1626
Floriane Grosset <fl.grosset@gmail.com>
2022-12-17 21:01:15 +0100

    add social networks links to contact page

--- src/component-pages/Contact/index.tsx
+++ src/component-pages/Contact/index.tsx
@@ -4,13 +4,26 @@ import Layout from 'components/Layout'
 import LayoutColumn from 'components/Layout/LayoutColumn'
 import Link from 'components/Link'
 import H1 from 'components/H1/H1'
+import Button from 'components/Button'
+import {Box} from '@mui/system'
+
+const SOCIAL_MEDIA = [
+	{
+		label: 'LinkedIn Profile',
+		link: 'https://www.linkedin.com/in/floriane-grosset-426435212/?locale=en_US',
+	},
+	{
+		label: 'GitHub',
+		link: 'https://github.com/grosset-floriane',
+	},
+]
 
 const Contact = () => {
 	return (
 		<Layout>
 			<LayoutColumn marginTop={0}>
 				<H1 description="Floriane's contact information">Contact</H1>
-				<Typography variant="bodyImpact">
+				<Typography variant="bodyImpact" component="p">
 					The easiest way to contact me is by email. Don’t hesitate to
 					drop me a line to say hi!
 					<br />
@@ -18,6 +31,30 @@ const Contact = () => {
 						contact@florianegrosset.com
 					</Link>
 				</Typography>
+
+				<Typography variant="h5" component="h3" mt={5}>
+					On social networks
+				</Typography>
+				<Box
+					mt={2}
+					display="flex"
+					gap={2}
+					component="ul"
+					ml={0}
+					style={{padding: 0}}
+				>
+					{SOCIAL_MEDIA.map(({label, link}) => (
+						<li key={label} style={{listStyle: 'none'}}>
+							<Button
+								href={link}
+								isLink
+								isExternal
+							>
+								{label}
+							</Button>
+						</li>
+					))}
+				</Box>
 			</LayoutColumn>
 		</Layout>
 	)
