690db50d5c19344b688672bab6cee3095427a021
Floriane Grosset <fl.grosset@gmail.com>
2022-12-18 00:29:43 +0100

    update SEO tags

--- src/components/H1/H1.tsx
+++ src/components/H1/H1.tsx
@@ -22,41 +22,22 @@ const H1 = ({
 		<>
 			<Helmet>
 				<title>{title || children} - Floriane Grosset</title>
+				<meta name="title" content={title || children} />
+				<meta name="description" content={description} />
+
 				<meta property="og:title" content={title || children} />
 				<meta property="og:description" content={description} />
 				<meta
 					property="og:url"
 					content={`https://florianegrosset.com${pathname}`}
 				/>
-                <meta property="og:site_name" content="TFloriane Grosset" />
-                <meta property="og:locale" content="en_UK" />
-				<meta property="og:type" content="website" />
-				<meta
-					property="og:image"
-					content="https://florianegrosset.com/assets/images/banner.jpg"
-				/>
-				<meta
-					property="og:image:alt"
-					content="React code poetry with a create function inside a Floriane Grosset component"
-				/>
-
-                {/* Twitter meta tags */}
-				<meta name="twitter:card" content="summary_large_image" />
+                
+				  {/* Twitter meta tags */}
 				<meta name="twitter:title" content={title || children} />
 				<meta
 					name="twitter:description"
 					content={description}
 				/>
-				<meta name="twitter:site" content="@FlosWebdesign" />
-				<meta
-					name="twitter:image"
-					content="https://florianegrosset.com/assets/images/banner.jpg"
-				/>
-                <meta
-					property="twitter:image:alt"
-					content="React code poetry with a create function inside a Floriane Grosset component"
-				/>
-				<meta name="twitter:creator" content="@FlosWebdesign" />
 			</Helmet>
 			<Typography
 				variant="h1"
