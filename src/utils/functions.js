a5fa76405f5f62a896381b9626f305d67a8fa219
Floriane Grosset <fl.grosset@gmail.com>
2023-01-05 20:40:35 +0100

    remove comments

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -2,11 +2,3 @@ export function generateImageSrc(projectSlug, image, size) {
 	if (!image && image.length < 1) return
 	return `/assets/images/${projectSlug}/${image.filename}_${image.category}_${size}.${image.extension}`
 }
-
-// export const renderLink = React.useMemo(
-//     () =>
-//       React.forwardRef((itemProps, ref) => {
-//         return <Link to={to} ref={ref} {...itemProps} role={undefined} />;
-//       }),
-//     [to],
-//   );
